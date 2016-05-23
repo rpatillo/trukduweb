8d398b13804b5842c5bf8f965f4d836389112694
Romain PATILLON <rpatillo@e2r9p6.42.fr>
2016-05-23 17:58:05 +0200

    bla

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
         <nav id="main-navigation">
             <ul>
                 <li><a href="/index.php">Home</a></li>
+                <li><a href="/Clogin.php">Login</a></li>
                 <li><a href="/photo.php">Take pictures</a></li>
                 <li><a href="#">Gallery</a></li>
                 <li><a href="#">My photos</a></li>
